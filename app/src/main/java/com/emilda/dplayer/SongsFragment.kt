f3feaea0db04a7ef5ff428b056187e64e205116f
Abin Babu <abinbabuabk@gmail.com>
2019-01-28 11:12:46 +0530

    Initial commit

---
+++ app/src/main/java/com/emilda/dplayer/SongsFragment.kt
@@ -0,0 +1,12 @@
+package com.emilda.dplayer
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SongsFragment : Fragment() {
+
+}
